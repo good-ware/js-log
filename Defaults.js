d4edc84399a0c35e582ef140e0b6e92868b0176d
dev-guy <bitbucket@terris.com>
2020-08-22 15:31:31 -0700

    Rename LoggerDefaults to Defaults

---
+++ Defaults.js
@@ -0,0 +1,8 @@
+class Defaults {}
+
+/**
+ * @description The maximum width of messages that are sent to loggers
+ */
+Defaults.maxMessageLength = 90;
+
+module.exports = Defaults;
--- RequestLogger.js
+++ RequestLogger.js
@@ -1,13 +1,14 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const requestPromise = require('request-promise');
 
-const Defaults = require('./LoggerDefaults');
+const Defaults = require('./Defaults');
 const FunctionLogger = require('./FunctionLogger');
 
 class RequestLogger {
   /**
-   * @description Sends two log entries for an HTTP request using the request-promise package: 'begin' and either 'end'
-   *  or 'error.' See FunctionLogger.execute() for a more detailed description.
+   * @description Sends two log entries for an HTTP request using the
+   * request-promise package: 'begin' and either 'end' or 'error.' See
+   * FunctionLogger.execute() for a more detailed description.
    *
    * @param {Object} logger
    * @param {Object} options request options
@@ -15,26 +16,24 @@ class RequestLogger {
    * @return {Promise} Value returned by request()
    */
   static request(logger, options, isError) {
-    let { url } = options;
-    if (options.qs && Object.keys(options.qs).length) url = `${url}?${JSON.stringify(options.qs)}`;
+    let {url} = options;
+    if (options.qs && Object.keys(options.qs).length)
+      url = `${url}?${JSON.stringify(options.qs)}`;
 
     // Strip the protocol
-    const snippet = `${options.method} ${url.replace(/^[^:]+:\/\//, '').substr(0, Defaults.maxMessageLength)}`;
+    const snippet = `${options.method} ${
+        url.replace(/^[^:]+:\/\//, '').substr(0, Defaults.maxMessageLength)}`;
 
     const begin = {
       options,
       message: `Begin: ${snippet}`,
     };
 
-    // @todo errorMessage should be a function that logs the status code in StatusCodeError
+    // @todo errorMessage should be a function that logs the status code in
+    // StatusCodeError
     return FunctionLogger.execute(
-      logger.child('http'),
-      () => requestPromise(options),
-      begin,
-      `End: ${snippet}`,
-      snippet,
-      isError
-    );
+        logger.child('http'), () => requestPromise(options), begin,
+        `End: ${snippet}`, snippet, isError);
   }
 }
 
